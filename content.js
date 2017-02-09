b8d9f5ce7bd33f178da8af56943456cb6512baf0
Nathan Dunn <nathanfdunn@gmail.com>
2017-02-08 22:11:18 -0500

    Added support for more than just <p> tags

--- content.js
+++ content.js
@@ -1,6 +1,26 @@
-var paragraphs = document.querySelectorAll('p');
+var paragraphs = [];
+
+var tags = ['p', 'a'];
+
+for (var i=0; i<tags.length; i++){
+	var nodes = document.querySelectorAll(tags[i]);
+	for (var j=0; j<nodes.length; j++){
+		paragraphs.push(nodes[j]);
+	}
+}
+console.log('psss',paragraphs);
 
 var regex = /\b[A-Z]{2,}\b/g;
+
+function stripDuplicates(array){
+	var out = [];
+	for (var i=0; i<array.length; i++){
+		if (out.indexOf(array[i]) === -1){
+			out.push(array[i]);
+		}
+	}
+	return out;
+}
 
 //Because sometimes definitions include spurious words
 function extractAcronymFromDefinition(acronym, definition){
@@ -23,12 +43,12 @@ function findAcronymDefinition(acronym, callback){
 		if (req.readyState!=4 || req.status!=200){return;}		//Mitigate duplicate request bug
 		var result = JSON.parse(req.responseText);
 		if (result.result_type != 'exact'){
-			callback(false);
+			callback('Debug: No result found');
 		} else {
-			callback(result.list[0].definition)
+			callback(result.list[0].definition);
 		}
 	}
-	req.open('GET', 'http://api.urbandictionary.com/v0/define?term=' + acronym, true);
+	req.open('GET', 'https://api.urbandictionary.com/v0/define?term=' + acronym, true);
 	req.send(null);
 }
 
@@ -38,7 +58,7 @@ function innerLoop(p, acronym){
 			definition = extractAcronymFromDefinition(acronym, definition);
 			p.textContent = p.textContent.replace(
 				new RegExp(acronym, 'g'),
-				acronym + ' ('+definition+')'
+				acronym + ' {'+definition+'}'
 			);
 		}
 	});
@@ -46,8 +66,9 @@ function innerLoop(p, acronym){
 
 for (var i=0; i<paragraphs.length; i++){
 	var p = paragraphs[i];
-	var acronyms = p.textContent.match(regex);
-	if (!acronyms){continue;}
+	console.log('should be 1',p);
+	console.log(p.textContent);
+	var acronyms = stripDuplicates(p.textContent.match(regex) || []);
 	for (var j=0; j<acronyms.length; j++){
 		var acronym = acronyms[j];
 		innerLoop(p, acronym);
